16c78cda591ae9d59bb709ed5e9c102cec09e6b7
alvinlukmanNH <galinx20gmail.com>
2018-10-01 22:07:14 +0700

    nilaiterbesardanterkecil

---
+++ src/NilaiTerbesarDanTerkecil.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama      : Alvin Lukman Nulhakim 
+ * Kelas     : IF3 
+ * NIM       : 10117095 
+ * Deskripsi : program ini menampilkan nilai mahasiswa terbesar dan terkecil
+ */
+public class NilaiTerbesarDanTerkecil {
+
+  
+    public static void main(String[] args) {
+       //        deklarasi variabel
+        String petugas;
+        int nMhs; //jumlah mahasiswa
+        int nBesar;
+        int nKecil;
+        int nilaiMhs;
+        Scanner scn = new Scanner(System.in);
+        
+        System.out.println("======Program Nilai Terbesar dan Terkecil Nilai "
+                + "Mahasiswa======");
+        
+        System.out.print("Masukkan Nama Petugas : ");
+        petugas = scn.next();
+        
+        System.out.print("Masukkan Banyaknya Mahasiswa : ");
+        nMhs = scn.nextInt();
+        
+//        inisialisasi variabel
+        nBesar = 0;
+        nKecil = 100;
+//        looping sesuai jumlahh mahasiswa
+        for (int i = 1; i <= nMhs; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa ke-"+i+" = ");
+            nilaiMhs = scn.nextInt();
+            
+            //menentukan nilai terbesar dan nilai terkecil
+            nBesar = ((nBesar<=nilaiMhs)?nilaiMhs:nBesar);
+            nKecil = ((nKecil>=nilaiMhs)?nilaiMhs:nKecil);
+        }
+        
+//        menampilkan hasil dari nilai terbesar dan nilai terkecil
+        System.out.print("\nNilai Terbesar adalah : "+ nBesar);
+        System.out.println("\nNilai Terkecil adalah : "+ nKecil);
+         
+    }
+    
+}
